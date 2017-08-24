5b0adcc6775aca8bc59612a6aa768c1eb9f889da
aker <mageaker@hotmail.com>
2017-08-24 15:16:00 +0900

    170824 Exceptions

---
+++ src/chap10/ClassCastExceptionExample.java
@@ -0,0 +1,18 @@
+package chap10;
+
+public class ClassCastExceptionExample {
+	public static void main(String[] args){
+		Dog dog = new Dog();
+		changeDog(dog);
+		
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+	public static void changeDog(Animal animal){
+		Dog dog = (Dog)animal;
+	}
+}
+
+class Animal{}
+class Dog extends Animal{}
+class Cat extends Animal{}
