d7d6fe892617ed5ec5931b36a33dd459a1c9ca96
aker <mageaker@hotmail.com>
2017-09-03 23:22:33 +0900

    170903 HashMap, Hashtable, Properties

--- src/chap15/Map/HashMapExample.java
+++ src/chap15/Map/HashMapExample.java
@@ -1,5 +1,46 @@
 package chap15.Map;
 
-public class HashMapExample {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+public class HashMapExample {
+	public static void main(String[] args){
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put("kim", 85);
+		map.put("lee", 90);
+		map.put("park", 80);
+		map.put("lee", 95);
+		System.out.println("총 Entry 수 : "+map.size());
+		
+		System.out.println("\tlee : "+map.get("lee"));
+		System.out.println();
+		
+		Set<String> keySet = map.keySet();
+		Iterator<String> keyIterator = keySet.iterator();
+		while(keyIterator.hasNext()){
+			String key = keyIterator.next();
+			Integer value = map.get(key);
+			System.out.println("\t"+key+" : "+value);
+		}
+		System.out.println();
+		
+		map.remove("lee");
+		System.out.println("총 Entry 수 : "+map.size());
+		Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+		Iterator<Map.Entry<String, Integer>> entryIterator = entrySet.iterator();
+		
+		while(entryIterator.hasNext()){
+			Map.Entry<String, Integer> entry = entryIterator.next();
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			System.out.print("\t"+key+" : "+value);
+		}
+		System.out.println();
+		
+		map.clear();
+		System.out.println("총 Entry 수 : "+map.size());
+	}
 }
--- src/chap15/Map/HashMapExample2.java
+++ src/chap15/Map/HashMapExample2.java
@@ -1,5 +1,15 @@
 package chap15.Map;
 
-public class HashMapExample2 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class HashMapExample2 {
+	public static void main(String[] args){
+		Map<Student, Integer> map = new HashMap<Student, Integer>();
+		
+		map.put(new Student(1, "kim"), 95);
+		map.put(new Student(1, "kim"), 95);
+		
+		System.out.println("총 Entry 수 : "+map.size());
+	}
 }
