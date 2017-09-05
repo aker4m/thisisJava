4330c63dc1ce510e7c6e7461759fa6f5858644c1
aker <mageaker@hotmail.com>
2017-09-05 12:10:35 +0900

    170905 reduce()

--- src/chap16/Reduce/ReductionExample.java
+++ src/chap16/Reduce/ReductionExample.java
@@ -1,5 +1,30 @@
 package chap16.Reduce;
 
-public class ReductionExample {
+import java.util.Arrays;
+import java.util.List;
 
+public class ReductionExample {
+	public static void main(String[] args){
+		List<Student> studentList = Arrays.asList(
+			new Student("hong", 92),
+			new Student("shin", 95),
+			new Student("kim", 88)
+		);
+		
+		int sum1 = studentList.stream()
+						.mapToInt(Student :: getScore)
+						.sum();
+		
+		int sum2 = studentList.stream()
+						.map(Student :: getScore)
+						.reduce((a,  b)-> a+b)
+						.get();
+		int sum3 = studentList.stream()
+						.map(Student :: getScore)
+						.reduce(0, (a,b)-> a+b);
+		
+		System.out.println("sum1 : "+sum1);
+		System.out.println("sum2 : "+sum2);
+		System.out.println("sum3 : "+sum3);
+	}
 }
--- src/chap16/Reduce/Student.java
+++ src/chap16/Reduce/Student.java
@@ -1,5 +1,18 @@
 package chap16.Reduce;
 
 public class Student {
-
+	private String name;
+	private int score;
+	
+	public Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public int getScore(){
+		return score;
+	}
 }
