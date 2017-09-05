7612c92ec80680658235af911e5e3ab2b533a42e
aker <mageaker@hotmail.com>
2017-09-05 09:35:59 +0900

    170905 flatMap, map, asXXXStream, boxed

--- src/chap16/Mapping/MapExample.java
+++ src/chap16/Mapping/MapExample.java
@@ -1,5 +1,18 @@
 package chap16.Mapping;
 
-public class MapExample {
+import java.util.Arrays;
+import java.util.List;
 
+public class MapExample {
+	public static void main(String[] args){
+		List<Student> studentList = Arrays.asList(
+			new Student("hong", 10), 
+			new Student("shin", 20),
+			new Student("yu", 30)
+		);
+		
+		studentList.stream()
+			.mapToInt(Student :: getScore)
+			.forEach(score -> System.out.println(score));
+	}
 }
