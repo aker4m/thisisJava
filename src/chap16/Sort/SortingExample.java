89800ccc2d3652773a3168789ba3262627838d35
aker <mageaker@hotmail.com>
2017-09-05 11:32:29 +0900

    170905 sorted(Comparator.reverseOrder())

--- src/chap16/Sort/SortingExample.java
+++ src/chap16/Sort/SortingExample.java
@@ -1,5 +1,31 @@
 package chap16.Sort;
 
-public class SortingExample {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
+public class SortingExample {
+	public static void main(String[] args){
+		IntStream intStream = Arrays.stream(new int[]{5, 3, 2, 1, 4});
+		intStream
+			.sorted()
+			.forEach(n -> System.out.print(n+", "));
+		System.out.println();
+		
+		List<Student> studentList = Arrays.asList(
+			new Student("hong", 30),
+			new Student("shin", 10),
+			new Student("yu", 20)
+		);
+		
+		studentList.stream()
+			.sorted()
+			.forEach(s -> System.out.print(s.getScore()+", "));
+		System.out.println();
+		
+		studentList.stream()
+			.sorted(Comparator.reverseOrder())
+			.forEach(s -> System.out.print(s.getScore()+", "));
+	}
 }
