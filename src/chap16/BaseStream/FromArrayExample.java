2c7737348edf2e0c9acaa19ee283dd9dbfab3bcf
aker <mageaker@hotmail.com>
2017-09-05 09:10:34 +0900

    170905 Stream, BaseStream

--- src/chap16/BaseStream/FromArrayExample.java
+++ src/chap16/BaseStream/FromArrayExample.java
@@ -1,5 +1,20 @@
 package chap16.BaseStream;
 
-public class FromArrayExample {
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+public class FromArrayExample {
+	public static void main(String[] args){
+		String[] strArray = { "hong", "shin", "kim"};
+		Stream<String> strStream = Arrays.stream(strArray);
+		strStream.forEach(a -> System.out.print(a+", "));
+		System.out.println();
+		
+		int[] intArray = {1, 2, 3, 4, 5};
+		IntStream intStream = Arrays.stream(intArray);
+		intStream.forEach(a -> System.out.print(a+", "));
+		System.out.println();
+		
+	}
 }
--- src/chap16/BaseStream/StreamPipelinesExample.java
+++ src/chap16/BaseStream/StreamPipelinesExample.java
@@ -1,5 +1,24 @@
 package chap16.BaseStream;
 
-public class StreamPipelinesExample {
+import java.util.Arrays;
+import java.util.List;
 
+public class StreamPipelinesExample {
+	public static void main(String[] args){
+		List<Member> list = Arrays.asList(
+			new Member("hong", Member.MALE, 30),
+			new Member("kim", Member.FEMAIL, 20),
+			new Member("shin", Member.MALE, 45),
+			new Member("park", Member.FEMAIL, 27)
+		);
+		
+		double ageAvg = list.stream()
+				.filter(m -> m.getSex() == Member.MALE)
+				.mapToInt(Member :: getAge)
+				.average()
+				.getAsDouble();
+		
+		System.out.println("ageAvg : "+ageAvg);
+		
+	}
 }
