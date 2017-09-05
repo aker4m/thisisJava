92bad692097b2e21add1a5aa1e9fe2fd2c0a2f00
aker <mageaker@hotmail.com>
2017-09-05 22:56:49 +0900

    170905 Console, SystemIn, SystemOut

--- src/chap18/Console/ConsoleExample.java
+++ src/chap18/Console/ConsoleExample.java
@@ -1,5 +1,20 @@
 package chap18.Console;
 
-public class ConsoleExample {
+import java.io.Console;
 
+public class ConsoleExample {
+	public static void main(String[] args){
+		Console console = System.console();
+		
+		System.out.print("id: ");
+		String id = console.readLine();
+		
+		System.out.print("password: ");
+		char[] charPass = console.readPassword();
+		String password = new String(charPass);
+		
+		System.out.println("----------------------------");
+		System.out.println(id);
+		System.out.println(password);
+	}
 }
--- src/chap18/Console/SystemInExample1.java
+++ src/chap18/Console/SystemInExample1.java
@@ -1,5 +1,43 @@
 package chap18.Console;
 
-public class SystemInExample1 {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class SystemInExample1 {
+	public static boolean flag = true;
+	public static void main(String[] args) throws IOException{
+		char inputChar;
+		while(flag){
+			System.out.println("== 메뉴 ==");
+			System.out.println("1. 예금 조회");
+			System.out.println("2. 예금 출금");
+			System.out.println("3. 예금 입금");
+			System.out.println("4. 종료 하기");
+			System.out.print("메뉴를 선택하세요: ");
+			
+			InputStream is = System.in;
+			inputChar = (char)is.read();
+			is.read();
+			print(inputChar);
+		}
+	}
+	public static void print(char inputChar){
+		switch(inputChar){
+		case '1':
+			System.out.println("예금 조회를 선택하셨습니다.");
+			break;
+		case '2':
+			System.out.println("예금 출금을 선택하셨습니다.");
+			break;
+		case '3':
+			System.out.println("예금 입금을 선택하셨습니다.");
+			break;
+		case '4':
+			System.out.println("종료 하기를 선택하셨습니다.");
+			flag=false;
+			break;
+		default:
+			System.out.println("잘못 입력하셨습니다.");
+		}
+	}
 }
