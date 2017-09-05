b8d99dea4048050f78116e43148ca67284200000
aker <mageaker@hotmail.com>
2017-09-05 13:49:24 +0900

    170905 Parallel Operation

--- src/chap16/Parallel/MaleStudentExample.java
+++ src/chap16/Parallel/MaleStudentExample.java
@@ -1,5 +1,25 @@
 package chap16.Parallel;
 
-public class MaleStudentExample {
+import java.util.Arrays;
+import java.util.List;
 
+import chap16.Collect.MaleStudent;
+import chap16.Collect.Student;
+
+public class MaleStudentExample {
+	public static void main(String[] args){
+		List<Student> totalList = Arrays.asList(
+			new Student("hong", 10, Student.Sex.MALE),
+			new Student("kim", 10, Student.Sex.FEMALE),
+			new Student("shin", 10, Student.Sex.MALE),
+			new Student("park", 10, Student.Sex.FEMALE)
+		);
+		
+		MaleStudent maleStudent = totalList.parallelStream()
+			.filter(s -> s.getSex() == Student.Sex.MALE)
+			.collect(MaleStudent :: new, MaleStudent :: accumulate, MaleStudent :: combine);
+		
+		maleStudent.getList().stream()
+			.forEach(s -> System.out.println(s.getName()));
+	}
 }
--- src/chap16/Parallel/SequencialVsParallelExample.java
+++ src/chap16/Parallel/SequencialVsParallelExample.java
@@ -1,5 +1,45 @@
 package chap16.Parallel;
 
-public class SequencialVsParallelExample {
+import java.util.Arrays;
+import java.util.List;
 
+public class SequencialVsParallelExample {
+	public static void work(int value){
+		try{
+			Thread.sleep(100);
+		}catch(InterruptedException e){}
+	}
+	
+	public static long testSequencial(List<Integer> list){
+		long start = System.nanoTime();
+		list.stream().forEach(a->work(a));
+		long end = System.nanoTime();
+		long runTime = end - start;
+		return runTime;
+	}
+	
+	public static long testParallel(List<Integer> list){
+		long start = System.nanoTime();
+		list.stream().parallel().forEach(a -> work(a));
+		long end = System.nanoTime();
+		long runTime = end - start;
+		return runTime;
+	}
+	
+	public static void main(String... args){
+		List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		long timeSequencial  = testSequencial(list);
+		
+		long timeParallel = testParallel(list);
+		
+		System.out.println("timeSequencial : "+timeSequencial);
+		System.out.println("timeParallel : "+timeParallel);
+		
+		if(timeSequencial < timeParallel){
+			System.out.println("성능 테스트 결과 : 순차 처리가 더 빠름");
+		}else{
+			System.out.println("성능 테스트 결과 : 병렬 처리가 더 빠름");
+		}
+	}
 }
